f4066eb2120f3416b170db75d370ad2f6da3a41d
Ondra <zachaondrej@gmail.com>
2017-03-09 22:57:09 +0100

    clean up, + proportion chart

--- 02-transform.R
+++ 02-transform.R
@@ -3,6 +3,8 @@ library(data.table)
 
 load("messages_dt.RData")
 
+
+# time --------------------------------------------------------------------
 # to ensure the date parsing works
 Sys.setlocale("LC_ALL", "English")
 # strptime should theoretically work as well, unfortunately it does not
@@ -10,34 +12,32 @@ dt[, time := parse_date_time(gsub("(^[a-zA-Z]*, |at | UTC\\+0[12]*)", "", meta),
                              "B!d!Y!I!M!p!")]
 # subtract 1 hour for summer time (UTC+02)
 dt[substr(meta, nchar(meta), nchar(meta)) == "2", time := time - 3600]
+dt[, meta := NULL]
+
+dt[, realday := as.Date(time - 3600 * 6)]  # because it's not tomorrow yet if i don't go to sleep
 
 
-# dt[, threadname := paste0(unique(ifelse(user %in% c("1445006818@facebook.com",
-#                                                     "Ondrej Zacha",
-#                                                     "Ondřej Zacha"),
-#                                         "", user)), 
-#                                  collapse = " "),
-#                           by = thread]
+# thread info -------------------------------------------------------------
 dt[, threadname := paste0(unique(user), collapse = "\n"),
    by = thread]
 dt[, threadname := gsub("(\\d*@facebook.com|Ond.ej Zacha)", 
                         "", threadname)]
-dt[, threadname := gsub("\n\n*", 
-                        "\n", threadname)]
+dt[, threadname := gsub("\n\n*", "\n", threadname)]
+dt[, threadname := gsub("(^\n|\n$)", "", threadname)]
 
 dt[, thread := gsub("@facebook.com", "", thread)]
 
-# for filtering
+# for filtering -----------------------------------------------------------
 dt[, plus1000 := .N > 1000L, by = thread]  # 43 threads
 dt[, plus5000 := .N > 5000L, by = thread]  # 15 threads
-
-dt[, realday := as.Date(time - 3600 * 6)]  # because it's not tomorrow yet if i don't go to sleep
-
 dt[, by_me := grepl("(1445006818@facebook.com|Ond.ej.Zacha)", user)]
 
+# dt[, group_chat := sapply(thread, function(x) length(gregexpr(",", x)[[1]])) > 1]
 
-dt[, meta := NULL]
 
-dt[grepl("\n", message)]
+# emoji -------------------------------------------------------------------
+
+# ???
+
 
 
--- 03-visualize.R
+++ 03-visualize.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 
+# total messages by week
 ggplot(dt[plus5000 == T, 
           .(.N, date = as.Date(min(time))), 
           by = .(year(time), 
@@ -12,6 +13,7 @@ ggplot(dt[plus5000 == T,
         legend.key.size = unit(2, "mm"),
         legend.spacing = unit(1, "mm"))
 
+# messages by week & thread
 ggplot(dt[plus5000 == T, 
           .(.N, date = as.Date(min(time))), 
           by = .(year(time), 
@@ -23,6 +25,7 @@ ggplot(dt[plus5000 == T,
   scale_fill_discrete(guide = FALSE) + 
   facet_wrap(~threadname, scales = "free_y")
 
+# after midnight
 ggplot(dt[plus5000 == T & hour(time) %between% list(0, 5) & time > '2012-01-01', 
           .(.N, date = as.Date(min(time))), 
           by = .(year(time), 
@@ -39,8 +42,9 @@ ggplot(dt[plus5000 == T & hour(time) %between% list(0, 5) & time > '2012-01-01',
 ggplot(dt[nchar(message) < 40000],
        aes(x = nchar(message))) +
   geom_histogram(bins = 100) +
-  scale_x_sqrt(breaks = c(10, 20, 50, 100, 500, 1000), labels = scales::comma) +
-  xlim(0, 1000)
+  scale_x_sqrt(breaks = c(10, 20, 50, 100, 500, 1000), 
+               labels = scales::comma, 
+               limits = c(0, 1000))
 
 # messages distribution by time
 ggplot(dt[plus5000 == T],
@@ -56,3 +60,10 @@ ggplot(dt[by_me == T,
   geom_point(alpha = 0.5) +
   geom_hline(yintercept = 24) +
   scale_y_continuous(labels = function(x) paste0(x %% 24, ":00"))
+
+# proportion of messages written by me
+ggplot(dt[plus1000 == T],
+       aes(x = threadname, fill = by_me)) +
+  geom_bar(position = "fill", stat = "count") +
+  geom_hline(yintercept = 0.5, linetype = "dashed") +
+  theme(axis.text = element_text(size = 7, angle = 65, hjust = 1))
